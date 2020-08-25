edc8052225d410bc24ebbd16589759ece69017de
Erika Sinkovics <erika.sinkovics@outlook.com>
2020-08-25 11:35:04 -0400

    Created MusiciansController

--- app/controllers/musicians_controller.rb
+++ app/controllers/musicians_controller.rb
@@ -1,7 +1,9 @@
 class MusiciansController < ApplicationController
   def index
+    @musicians = User.where("is_musician = true")
   end
 
   def show
+    @musician = User.find(params[:id])
   end
 end
