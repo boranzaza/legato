583006376a17876069f7d3e56930202138d56cfc
Michel-Alexandre Broekaert <contact@broekaert.space>
2020-08-27 15:11:17 -0400

    destroy method in booking controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -2,16 +2,24 @@ class BookingsController < ApplicationController
   #line below will call #set_booking method before specified actions
   before_action :set_booking, only: [:show, :edit, :update, :approve, :reject]
 
+
   def index
-    @bookings = Booking.all.order("created_at DESC")
+    @bookings = Booking.where(musician_id: current_user.id).reverse
     @bookings.each do |booking|
       booking.status
     end
   end
 
+  def show
+    a = Booking.find(params[:id])
+  end
+
   def new
     @customer_id = User.find(params[:user_id])
     @booking = Booking.new
+  end
+
+  def edit
   end
 
   def create
@@ -24,23 +32,9 @@ class BookingsController < ApplicationController
     end
   end
 
-  def index
-    @bookings = Booking.where(musician_id: current_user.id).reverse
-    # @current_bookings = Booking.where daytime
-    @bookings.each do |booking|
-      booking.status
-    end
-  end
-  
-  def show
-  end
-
   def update
     @booking.update(booking_params)
     redirect_to bookings_path
-  end
-
-  def edit
   end
 
   def approve
@@ -55,6 +49,24 @@ class BookingsController < ApplicationController
     redirect_to bookings_path
   end
 
+def destroy
+  @booking = Booking.find(params[:id])
+  @booking.destroy
+
+  redirect_to bookings_path
+end
+
+
+
+#   # 1. Display list with indices
+#     display_tasks
+#     # 2. Ask user for index
+#     index = @view.ask_user_for_index
+#     # 3. Remove from repository
+#     @repository.remove(index)
+
+# no need for app/views/restaurants/destroy.html.erb
+
   private
 
   def booking_params
@@ -65,3 +77,10 @@ class BookingsController < ApplicationController
     @booking = Booking.find(params[:id])
   end
 end
+
+# def index
+#     @bookings = Booking.all.order("created_at DESC")
+#     @bookings.each do |booking|
+#       booking.status
+#     end
+#   end
