6a3cf48e583e279997a84198160fc320d3b2fc38
Erika Sinkovics <erika.sinkovics@outlook.com>
2020-08-28 11:55:48 -0400

    Review routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users do
     resources :bookings, only: [:new, :create]
+    resources :reviews, only: [:new, :create]
   end
   resources :bookings, except: [:new, :create]
+  resources :reviews, except: [:new, :create]
 
   patch '/approve', to: 'bookings#approve', as: 'approve'
   patch '/reject', to: 'bookings#reject', as: 'reject'
